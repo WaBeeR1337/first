0a7dce0a89f29dae357f0e9d761f2d81df5057b6
WaBeeR1337 <shantarenko1999@gmail.com>
2024-04-28 13:07:17 +0300

    Lukashenko site

---
+++ index.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "ты моя умничка<3";
+  }
+  function myFunction1() {
+    document.getElementById("demo1").innerHTML = "Я записао тебя в список пидоров";
+  }
